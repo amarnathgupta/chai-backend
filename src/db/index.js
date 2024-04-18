1556a6bdf3aba0d54efc386ee753c2cf40407372
Amar Nath Gupta <ang.amarnathgupta@gmail.com>
2024-04-18 15:29:20 +0530

    utils code discussion

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,10 @@ const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log(`MongoDB connected || DB Host: ${connectionInstance.connection.host}`)
-        // application.on('error', ())
+        application.on('error', () => {
+            console.log("Express error while connecting to the database. ERROR : ",error);
+            throw error;
+        })
     } catch (error) {
         console.log("MongoDB connection error ",error);
         process.exit(1);
